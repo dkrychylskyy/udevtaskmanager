8907866724b3c4c97d676c6923608b3c10044d3d
dimitry.krychylskyy <d.krychylskyy@gmail.com>
2018-06-22 12:19:08 +0200

    add independence GSON Parsing JSON est pret Creation des utilisateurs de JSON est pret

---
+++ src/test/groovy/com/udev/tskmngr/CreationUsersFromJSONServiceSpec.groovy
@@ -0,0 +1,18 @@
+package com.udev.tskmngr
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class CreationUsersFromJSONServiceSpec extends Specification implements ServiceUnitTest<CreationUsersFromJSONService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
